b5b7a515bf5a990b01a7b31a1fd3085d774cc6f8
Ugon <wojciech.pachuta01@gmail.com>
2015-11-25 08:47:35 +0100

    fixet maven packaging, hibernate template

--- app/core/src/main/java/pl/edu/agh/iisg/to/to2project/core/ApplicationMain.java
+++ app/core/src/main/java/pl/edu/agh/iisg/to/to2project/core/ApplicationMain.java
@@ -5,15 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import pl.edu.agh.iisg.to.to2project.domain.Account;
+import pl.edu.agh.iisg.to.to2project.service.AccountService;
+import pl.edu.agh.iisg.to.to2project.service.CategoryService;
+import pl.edu.agh.iisg.to.to2project.service.TransactionService;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.ALL;
 
 /**
- * @author Bartłomiej Grochal.
+ * @author Bartłomiej Grochal, Wojciech Pachuta
  */
+@SpringBootApplication
+@ImportResource({"classpath:persistenceContext.xml"})
+@ComponentScan({"pl.edu.agh.iisg.to.to2project"})
 public class ApplicationMain extends Application {
 
     private Stage primaryStage;
@@ -45,6 +58,16 @@ public class ApplicationMain extends Application {
     }
 
     public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(ApplicationMain.class, args);
+
+        AccountService accountService = (AccountService) ctx.getBean("accountServiceImpl");
+        CategoryService categoryService = (CategoryService) ctx.getBean("categoryServiceImpl");
+        TransactionService transactionService = (TransactionService) ctx.getBean("transactionServiceImpl");
+
+        System.out.println("dupa\n\n");
+
+        accountService.save(new Account("Dupa", new BigDecimal(10)));
+
         launch(args);
     }
 }
