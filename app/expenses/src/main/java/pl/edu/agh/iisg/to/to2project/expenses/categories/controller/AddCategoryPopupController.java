a52956b52e61c328c4a1174fd92a432237acb3d5
bgrochal <bartek.grochal@gmail.com>
2015-11-26 18:14:56 +0100

    Created Popup Windows for Expenses/Transactions and Expenses/Categories. Little changes for Expenses/Accounts.

---
+++ app/expenses/src/main/java/pl/edu/agh/iisg/to/to2project/expenses/categories/controller/AddCategoryPopupController.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.iisg.to.to2project.expenses.categories.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import pl.edu.agh.iisg.to.to2project.expenses.common.controller.PopupController;
+
+/**
+ * @author Bartłomiej Grochal
+ */
+public class AddCategoryPopupController extends PopupController {
+
+    @FXML
+    @Override
+    protected void handleOKButtonClick(ActionEvent actionEvent) {
+    }
+}
