a469ffb27bbad5b4e562a372a03ce0d8c742a9e2
bgrochal <bartek.grochal@gmail.com>
2015-12-08 21:43:42 +0100

    Deleting Transactions implemented.

--- app/src/main/java/pl/edu/agh/iisg/to/to2project/app/expenses/transactions/controller/DeleteTransactionPopupController.java
+++ app/src/main/java/pl/edu/agh/iisg/to/to2project/app/expenses/transactions/controller/DeleteTransactionPopupController.java
@@ -2,10 +2,15 @@ package pl.edu.agh.iisg.to.to2project.app.expenses.transactions.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import pl.edu.agh.iisg.to.to2project.app.expenses.common.controller.PopupController;
 import pl.edu.agh.iisg.to.to2project.domain.AbstractTransaction;
+import pl.edu.agh.iisg.to.to2project.domain.ExternalTransaction;
+import pl.edu.agh.iisg.to.to2project.domain.InternalTransaction;
+import pl.edu.agh.iisg.to.to2project.service.ExternalTransactionService;
+import pl.edu.agh.iisg.to.to2project.service.InternalTransactionService;
 
 /**
  * @author Bartłomiej Grochal
@@ -14,13 +19,31 @@ import pl.edu.agh.iisg.to.to2project.domain.AbstractTransaction;
 @Scope("prototype")
 public class DeleteTransactionPopupController extends PopupController {
 
+    @Autowired
+    private InternalTransactionService internalTransactionService;
+
+    @Autowired
+    private ExternalTransactionService externalTransactionService;
+
+    private AbstractTransaction deletedTransaction;
+
+
+
     @FXML
     @Override
     protected void handleOKButtonClick(ActionEvent actionEvent) {
+        if(deletedTransaction instanceof ExternalTransaction) {
+            externalTransactionService.remove((ExternalTransaction) deletedTransaction);
+        }
+        else if (deletedTransaction instanceof InternalTransaction) {
+            internalTransactionService.remove((InternalTransaction) deletedTransaction);
+        }
 
+        dialogStage.close();
     }
 
     public void deleteTransaction(AbstractTransaction transaction) {
+        deletedTransaction = transaction;
         showDialog();
     }
 }
