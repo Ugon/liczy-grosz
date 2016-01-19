6790cb9d28a22e387910ef2cd457384faa3e1bdc
patrykks <patrykkskalski@gmail.com>
2016-01-19 19:51:11 +0100

    Database Improvments

--- budget-persistence/src/main/java/pl/edu/agh/iisg/to/to2project/budget_persistence/BudgetPersistenceManager.java
+++ budget-persistence/src/main/java/pl/edu/agh/iisg/to/to2project/budget_persistence/BudgetPersistenceManager.java
@@ -18,6 +18,7 @@ public class BudgetPersistenceManager {
     static {
         register();
         openConnection();
+        initializeTables();
     }
 
     private static void register()
@@ -46,7 +47,7 @@ public class BudgetPersistenceManager {
         Integer mewPlanId = null;
         //STEP 4: Execute a query
         System.out.println("Creating statement...");
-        String query = "SELECT max(id) as id  FROM Plan";
+        String query = "SELECT max(id) as id  FROM PLAN";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
 
         ResultSet rs = preparedStmt.executeQuery();
@@ -72,7 +73,7 @@ public class BudgetPersistenceManager {
         Integer newMonthId = null;
         //STEP 4: Execute a query
         System.out.println("Creating statement...");
-        String query = "SELECT max(id) as id  FROM Date";
+        String query = "SELECT max(id) as id  FROM DATE";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
 
         ResultSet rs = preparedStmt.executeQuery();
@@ -90,7 +91,7 @@ public class BudgetPersistenceManager {
         }
 
         preparedStmt.close();
-        query = "INSERT INTO Date " +
+        query = "INSERT INTO DATE " +
                 "VALUES( ? , ? , ?)";
         preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1, newMonthId);
@@ -109,7 +110,7 @@ public class BudgetPersistenceManager {
         Integer monthId = getDateId(year, month);
         if (monthId == null) monthId = insertNewMonth(year, month);
         Integer newPlanId = getMaxPlanId();
-        String query = "INSERT INTO Plan " +
+        String query = "INSERT INTO PLAN " +
                 "VALUES(?, ? , ? , ?, ?)";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1, newPlanId);
@@ -127,7 +128,7 @@ public class BudgetPersistenceManager {
         if(doesPlanForMonthExist(categoryName, year, month))
         {
             Integer dateId = getDateId(year, month);
-            String query = "update Plan set EarningPlanValue = ? where DateId = ? AND name = ?";
+            String query = "update PLAN set EarningPlanValue = ? where DateId = ? AND name = ?";
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             preparedStmt.setDouble(1, newEarningValue);
             preparedStmt.setInt   (2, dateId);
@@ -145,7 +146,7 @@ public class BudgetPersistenceManager {
         if(doesPlanForMonthExist(categoryName, year, month))
         {
             int monthId = getDateId(year, month);
-            String query = "update Plan set SpendingPlanValue = ? WHERE DateId = ? AND name = ?";
+            String query = "update PLAN set SpendingPlanValue = ? WHERE DateId = ? AND name = ?";
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             preparedStmt.setDouble(1, newSpendingValue);
             preparedStmt.setInt   (2, monthId);
@@ -162,7 +163,7 @@ public class BudgetPersistenceManager {
     public static void updateCategoryName(String oldCategoryName, String newCategoryName) throws SQLException
     {
         System.out.println("Old name" + oldCategoryName + " newName" + newCategoryName);
-        String query = "update Plan set name = ? WHERE name = ?";
+        String query = "update PLAN set name = ? WHERE name = ?";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setString(1, newCategoryName);
         preparedStmt.setString(2, oldCategoryName);
@@ -176,7 +177,7 @@ public class BudgetPersistenceManager {
         Integer monthId = null;
         //STEP 4: Execute a query
         System.out.println("Creating statement...");
-        String query = "SELECT id  FROM Date  WHERE Year = ? and Month = ?";
+        String query = "SELECT id  FROM DATE  WHERE Year = ? and Month = ?";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1,year);
         preparedStmt.setInt(2, month);
@@ -204,9 +205,9 @@ public class BudgetPersistenceManager {
 
         String query;
         if(isSpending)
-            query = "SELECT SpendingPlanValue as planValue FROM Plan WHERE DateId = ? and name = ?";
+            query = "SELECT SpendingPlanValue as planValue FROM PLAN WHERE DateId = ? and name = ?";
         else
-            query = "SELECT EarningPlanValue as planValue FROM Plan WHERE DateId = ? and name = ?";
+            query = "SELECT EarningPlanValue as planValue FROM PLAN WHERE DateId = ? and name = ?";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1, dateId);
         preparedStmt.setString(2, categoryName);
@@ -228,11 +229,11 @@ public class BudgetPersistenceManager {
         List<Plan> plans = new ArrayList<Plan>();
         //STEP 4: Execute a query
         System.out.println("Creating statement...");
-        String query = "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM Plan as p JOIN Date as d on d.id = p.DateId WHERE d.Year > ? and d.Year < ? " +
+        String query = "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM PLAN as p JOIN DATE as d on d.id = p.DateId WHERE d.Year > ? and d.Year < ? " +
                 " UNION " +
-                "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM Plan as p JOIN Date as d on d.id = p.DateId WHERE Year = ? and Month >= ?" +
+                "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM PLAN as p JOIN DATE as d on d.id = p.DateId WHERE Year = ? and Month >= ?" +
                 " UNION " +
-                "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM Plan as p JOIN Date as d on d.id = p.DateId WHERE Year = ? and Month <= ?";
+                "SELECT p.name, p.SpendingPlanValue, p.EarningPlanValue, d.Year, d.Month FROM PLAN as p JOIN DATE as d on d.id = p.DateId WHERE Year = ? and Month <= ?";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1,yearFrom);
         preparedStmt.setInt(2,yearTo);
@@ -270,7 +271,7 @@ public class BudgetPersistenceManager {
         System.out.println("Creating statement...");
         Integer dateId = getDateId(year, month);
         if (dateId == null) return false;
-        String query = "SELECT id FROM Plan WHERE DateId = ? and name = ?";
+        String query = "SELECT id FROM PLAN WHERE DateId = ? and name = ?";
 
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1,dateId);
@@ -297,7 +298,7 @@ public class BudgetPersistenceManager {
     public static boolean doesPlanForCategoryExist(String categoryName) throws SQLException {
         boolean categoryExists;
 
-        String query = "SELECT id FROM Plan WHERE name = ? and SpendingPlanValue != 0 or EarningPlanValue != 0 ";
+        String query = "SELECT id FROM PLAN WHERE name = ? and SpendingPlanValue != 0 or EarningPlanValue != 0 ";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setString(1, categoryName);
 
@@ -317,7 +318,7 @@ public class BudgetPersistenceManager {
     public static boolean doesCategoryExist(String categoryName) throws SQLException {
         boolean categoryExists;
 
-        String query = "SELECT id FROM Plan WHERE name = ?";
+        String query = "SELECT id FROM PLAN WHERE name = ?";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setString(1, categoryName);
 
@@ -334,6 +335,98 @@ public class BudgetPersistenceManager {
         if (rs!=null) rs.close();
         return categoryExists;
     }
+
+    private static void createPlanTable()
+    {
+        String query =  "CREATE TABLE PLAN ( "+
+                "	id INT NOT NULL, " +
+                "name VARCHAR(255) NOT NULL, " +
+                "EarningPlanValue DECIMAL NULL, " +
+                "SpendingPlanValue DECIMAL NULL, " +
+                "DateId INT NOT NULL, " +
+                " PRIMARY KEY (id,name), " +
+                "CONSTRAINT `fk_Plan_1` " +
+                "FOREIGN KEY (`DateId`) "+
+                "REFERENCES DATE (`id`) " +
+                "ON DELETE NO ACTION "+
+                "ON UPDATE NO ACTION);";
+
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(query);
+            preparedStmt.execute();
+            preparedStmt.close();
+        } catch (SQLException e) {
+            System.out.println("Error during creating Plan Table");
+            e.printStackTrace();
+        }
+
+        System.out.println("Created table Plan in given database...");
+    }
+
+    private static void createDateTable()
+    {
+        String query = "CREATE TABLE DATE " +
+                "(id INT not NULL, " +
+                " Year INT not NULL, " +
+                " Month INT not NULL, " +
+                " PRIMARY KEY ( id ))";
+
+        String alter_table = "ALTER TABLE DATE ADD UNIQUE unique_index (Year, Month);";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(query);
+            preparedStmt.executeUpdate();
+            preparedStmt.close();
+        } catch (SQLException e) {
+            System.out.println("Error during creating Date Table");
+            e.printStackTrace();
+        }
+
+        System.out.println("Created table Date in given database...");
+    }
+
+    private static void addDateUniqueIndex()
+    {
+        String alter_table = "ALTER TABLE DATE ADD UNIQUE unique_index (Year, Month);";
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = conn.prepareStatement(alter_table);
+            preparedStmt.executeUpdate();
+            preparedStmt.close();
+        } catch (SQLException e) {
+            System.out.println("Error during creating Date Table");
+            e.printStackTrace();
+        }
+
+        System.out.println("Created table Date in given database...");
+    }
+
+
+    public static void initializeTables()
+    {
+        boolean dateExist = false;
+        boolean planExist = false;
+        DatabaseMetaData md = null;
+        try {
+            md = conn.getMetaData();
+            ResultSet rs = md.getTables(null, null, "%", null);
+            while (rs.next()) {
+                String tableName = rs.getString(3);
+                if (tableName.equals("DATE"))
+                    dateExist = true;
+                if (tableName.equals("PLAN"))
+                    planExist = true;
+            }
+            if (!dateExist) createDateTable();
+            if (!dateExist) addDateUniqueIndex();
+            if (!planExist) createPlanTable();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void closeConnection() throws SQLException
     {
